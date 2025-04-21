cf6eb639247eb77d71a5e70af18964da5b6a1ffc
private210 <pname210@gmail.com>
2025-04-21 19:47:36 +0700

    model

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kelas extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_kelas',
+        'tahun_ajaran',
+        'wali_kelas_id',
+    ];
+
+    public function waliKelas()
+    {
+        return $this->belongsTo(User::class, 'wali_kelas_id');
+    }
+
+    public function siswa()
+    {
+        return $this->hasMany(Siswa::class);
+    }
 }
